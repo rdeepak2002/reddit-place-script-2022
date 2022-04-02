08272f4ac46d1fddc056b3d5c5a05fb74bd45b2e
ixi2101 <alpha@omega>
2022-04-02 19:56:09 +0100

    new colours mapping

--- mappings.py
+++ mappings.py
@@ -1,15 +1,23 @@
 color_map = {
+    "#BE0039": 1,  # Berry
     "#FF4500": 2,  # bright red
     "#FFA800": 3,  # orange
     "#FFD635": 4,  # yellow
     "#00A368": 6,  # darker green
+    "#00CC78": 7,   # seafoam green
     "#7EED56": 8,  # lighter green
+    "#00756F": 9,   #teal
+    "#009EAA": 10, #
     "#2450A4": 12,  # darkest blue
     "#3690EA": 13,  # medium normal blue
     "#51E9F4": 14,  # cyan
+    "#493AC1": 15,
+    "#6A5CFF": 16,
     "#811E9F": 18,  # darkest purple
     "#B44AC0": 19,  # normal purple
+    "#FF3881": 22,   # more pink
     "#FF99AA": 23,  # pink
+    "#6D482F": 24,   # brwn
     "#9C6926": 25,  # brown
     "#000000": 27,  # black
     "#898D90": 29,  # grey
@@ -23,13 +31,20 @@ name_map = {
     3: "Orange",
     4: "Yellow",
     6: "Dark Green",
+    7: "Green",
     8: "Light Green",
+    9: "Teal",
+    10: "Slate",
     12: "Dark Blue",
     13: "Blue",
     14: "Cyan",
+    15: "Purplish",
+    16: "light purple",
     18: "Dark Purple",
     19: "Purple",
-    23: "Pink",
+    22: "Very Pink",
+    23: "Salmon",
+    24: "Brownny",
     25: "Brown",
     27: "Black",
     29: "Grey",
