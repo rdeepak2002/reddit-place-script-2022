5736179d5a53f5ca2379c5084ea74d5334f5db38
aa <alpha@omega>
2022-04-02 12:03:51 +0100

    move to seperate file

---
+++ mappings.py
@@ -0,0 +1,38 @@
+color_map = {
+    "#FF4500": 2,  # bright red
+    "#FFA800": 3,  # orange
+    "#FFD635": 4,  # yellow
+    "#00A368": 6,  # darker green
+    "#7EED56": 8,  # lighter green
+    "#2450A4": 12,  # darkest blue
+    "#3690EA": 13,  # medium normal blue
+    "#51E9F4": 14,  # cyan
+    "#811E9F": 18,  # darkest purple
+    "#B44AC0": 19,  # normal purple
+    "#FF99AA": 23,  # pink
+    "#9C6926": 25,  # brown
+    "#000000": 27,  # black
+    "#898D90": 29,  # grey
+    "#D4D7D9": 30,  # light grey
+    "#FFFFFF": 31,  # white
+}
+
+# map of pixel color ids to verbose name (for debugging)
+name_map = {
+    2: "Bright Red",
+    3: "Orange",
+    4: "Yellow",
+    6: "Dark Green",
+    8: "Light Green",
+    12: "Dark Blue",
+    13: "Blue",
+    14: "Cyan",
+    18: "Dark Purple",
+    19: "Purple",
+    23: "Pink",
+    25: "Brown",
+    27: "Black",
+    29: "Grey",
+    30: "Light Grey",
+    31: "White",
+}
