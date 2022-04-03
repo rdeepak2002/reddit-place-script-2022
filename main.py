8c20188aac492ae4f8d939072c696f7ce63b3f2c
ixi2101 <alpha@omega>
2022-04-03 11:40:57 +0100

    run black

--- main.py
+++ main.py
@@ -32,26 +32,25 @@ class PlaceClient:
         # In seconds
         self.delay_between_launches = (
             self.json_data["thread_delay"]
-            if "thread_delay" in self.json_data and
-            self.json_data["thread_delay"] is not None
+            if "thread_delay" in self.json_data
+            and self.json_data["thread_delay"] is not None
             else 3
         )
         self.unverified_place_frequency = (
             self.json_data["unverified_place_frequency"]
-            if "unverified_place_frequency" in self.json_data and
-            self.json_data["unverified_place_frequency"] is not None
+            if "unverified_place_frequency" in self.json_data
+            and self.json_data["unverified_place_frequency"] is not None
             else False
         )
         self.proxies = (
             self.GetProxies(self.json_data["proxies"])
-            if "proxies" in self.json_data and
-            self.json_data["proxies"] is not None
+            if "proxies" in self.json_data and self.json_data["proxies"] is not None
             else None
         )
         self.compactlogging = (
             self.json_data["compact_logging"]
-            if "compact_logging" in self.json_data and
-            self.json_data["compact_logging"] is not None
+            if "compact_logging" in self.json_data
+            and self.json_data["compact_logging"] is not None
             else True
         )
 
@@ -197,7 +196,9 @@ class PlaceClient:
             waitTime = math.floor(
                 response.json()["errors"][0]["extensions"]["nextAvailablePixelTs"]
             )
-            logger.error("Thread #{} : Failed placing pixel: rate limited", thread_index)
+            logger.error(
+                "Thread #{} : Failed placing pixel: rate limited", thread_index
+            )
         else:
             waitTime = math.floor(
                 response.json()["data"]["act"]["data"][0]["data"][
@@ -353,7 +354,10 @@ class PlaceClient:
                 x = 0
 
             if y >= self.image_size[1]:
-                logging.info("Thread #{} : All pixels correct, trying again in 10 seconds... ", index)
+                logging.info(
+                    "Thread #{} : All pixels correct, trying again in 10 seconds... ",
+                    index,
+                )
 
                 time.sleep(10)
 
@@ -450,14 +454,15 @@ class PlaceClient:
 
                 # refresh access token if necessary
                 if (
-                    len(self.access_tokens) == 0 or
-                    len(self.access_token_expires_at_timestamp) == 0 or
+                    len(self.access_tokens) == 0
+                    or len(self.access_token_expires_at_timestamp) == 0
+                    or
                     # index in self.access_tokens
-                    index not in self.access_token_expires_at_timestamp or
-                    (
-                        self.access_token_expires_at_timestamp.get(index) and
-                        current_timestamp >=
+                    index not in self.access_token_expires_at_timestamp
+                    or (
                         self.access_token_expires_at_timestamp.get(index)
+                        and current_timestamp
+                        >= self.access_token_expires_at_timestamp.get(index)
                     )
                 ):
                     if not self.compactlogging:
@@ -503,7 +508,7 @@ class PlaceClient:
                     data_str = (
                         BeautifulSoup(r.content, features="html.parser")
                         .find("script", {"id": "data"})
-                        .contents[0][len("window.__r = "): -1]
+                        .contents[0][len("window.__r = ") : -1]
                     )
                     data = json.loads(data_str)
                     response_data = data["user"]["session"]
@@ -534,8 +539,8 @@ class PlaceClient:
 
                 # draw pixel onto screen
                 if self.access_tokens.get(index) is not None and (
-                    current_timestamp >= next_pixel_placement_time or
-                    self.first_run_counter <= index
+                    current_timestamp >= next_pixel_placement_time
+                    or self.first_run_counter <= index
                 ):
 
                     # place pixel immediately
@@ -575,7 +580,7 @@ class PlaceClient:
                         pixel_y_start,
                         pixel_color_index,
                         canvas,
-                        index
+                        index,
                     )
 
                     current_r += 1
