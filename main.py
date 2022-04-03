bd26db11cb97e77fb5c5a1e2611b1dc2d38b4cfa
Ph-ill <phill.scargill@gmail.com>
2022-04-03 13:09:47 +0100

    user-Agent Fix & old design user fix
    Stops reddit from blocking requests from the python user-agent by replacing it with the default chrome desktop user agent, as well as fixed bugs that broke the script when the user account supplied had opted out of the reddit redesign

--- main.py
+++ main.py
@@ -513,6 +513,7 @@ class PlaceClient:
                         exit(1)
 
                     client = requests.Session()
+                    client.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'})
                     r = client.get("https://www.reddit.com/login")
                     login_get_soup = BeautifulSoup(r.content, "html.parser")
                     csrf_token = login_get_soup.find("input", {"name": "csrf_token"})[
@@ -536,7 +537,7 @@ class PlaceClient:
                     else:
                         print("Authorization successful!")
                     print("Obtaining access token...")
-                    r = client.get("https://www.reddit.com/")
+                    r = client.get("https://new.reddit.com/")
                     data_str = (
                         BeautifulSoup(r.content, features="html.parser")
                         .find("script", {"id": "data"})
