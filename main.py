8e83df68765fda3b57aaf03dfea82b62ce4f22aa
Plantchant <74047628+Plantchant@users.noreply.github.com>
2022-04-03 03:45:36 -0400

    Fixed Canvas Appearing Solid Black For X>1000
    Fixed an issue with getting the canvas from reddit.
    
    The x>1000 pixels were showing up as entirely black. It now works.
    
    You can verify this yourself by adding "new_img.show()" above the return.

--- main.py
+++ main.py
@@ -260,7 +260,8 @@ class PlaceClient:
                 }
             )
         )
-
+        ws.recv()
+        
         image_sizex = 2
         image_sizey = 1
 
@@ -289,7 +290,7 @@ class PlaceClient:
                     }
                 )
             )
-            file = ""
+            ws.recv()
             while True:
                 temp = json.loads(ws.recv())
                 if temp["type"] == "data":
@@ -320,8 +321,8 @@ class PlaceClient:
             new_img.paste(img, (x_offset, 0))
             x_offset += img.size[0]
 
-        logger.info("Got image: {}", file)
-
+        logger.info("Got image.")
+        # new_img.show()
         return new_img
 
     def get_unset_pixel(self, boardimg, x, y, index):
