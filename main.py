ea5f59f461ceae6172339aea6675487e25c2d614
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2022-04-03 11:08:19 -0400

    Fix transparent / skipover pixels not working again

--- main.py
+++ main.py
@@ -397,11 +397,11 @@ class PlaceClient:
                     "{}, {}, {}, {}",
                     pix2[x + self.pixel_x_start, y + self.pixel_y_start],
                     new_rgb,
-                    new_rgb != (69, 42, 0),
+                    target_rgb != (69, 42, 0),
                     pix2[x, y] != new_rgb,
                 )
 
-                if new_rgb != (69, 42, 0):
+                if target_rgb != (69, 42, 0):
                     logger.debug(
                         "Thread #{} : Replacing {} pixel at: {},{} with {} color",
                         index,
@@ -412,7 +412,7 @@ class PlaceClient:
                     )
                     break
                 else:
-                    logger.info("TransparrentPixel")
+                    logger.info("Transparent Pixel at {}, {} skipped", x + self.pixel_x_start, y + self.pixel_y_start)
             x += 1
             loopedOnce = True
         return x, y, new_rgb
